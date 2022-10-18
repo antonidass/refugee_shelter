42a05ff08598006399422d68661758ba4b2b9c84
Антон <akrikoff@gmail.com>
2022-10-18 18:09:11 +0300

    set up docker-compose and remove trash

--- backend3/RefugeeShelter/src/main/java/com/example/refugeeshelter/repositories/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.refugeeshelter.repositories;
-
-import com.example.refugeeshelter.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
