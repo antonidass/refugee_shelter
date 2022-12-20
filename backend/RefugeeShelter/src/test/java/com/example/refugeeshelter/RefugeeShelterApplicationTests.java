dac3ecb0889cb0842a77f0e25ab29903dc40d9eb
Антон <akrikoff@gmail.com>
2022-12-20 22:11:37 +0300

    add

--- backend/RefugeeShelter/src/test/java/com/example/refugeeshelter/RefugeeShelterApplicationTests.java
+++ backend/RefugeeShelter/src/test/java/com/example/refugeeshelter/RefugeeShelterApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.refugeeshelter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RefugeeShelterApplicationTests {
 }
 //    $ allure serve target/allure-results --host localhost --port 9999
